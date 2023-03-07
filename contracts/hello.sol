358caddc73a04a2149c2cc5a96fb7d6c4ae4d7e5
akinbo_emmanuel <akinbo.emmanuel@lmu.edu.ng>
2023-03-07 05:10:12 +0100

    NFT Counter Contract

---
+++ contracts/hello.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract MyFirstContract{
+    // string public hey = "Hey, Emmanuel";
+    // uint256 public no = 4;
+
+    string public hey;
+    uint256 public no;
+
+    // constructor(string memory _hey, uint _no){
+    //     hey = _hey;
+    //     no = _no;
+    // }
+
+    // function addInfo(string memory _hey, uint _no) public {
+    //     hey = _hey;
+    //     no = _no;
+    // }
+}
