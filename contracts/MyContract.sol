9498c2f168bb6c9a8201f45774de091f8622c904
akinbo_emmanuel <akinbo.emmanuel@lmu.edu.ng>
2023-03-22 17:47:26 +0100

    Arrays

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MyContract {
+contract Variables {
     //State Variables
     int256 public myInt = 1;
     uint public myUint = 1;
@@ -24,5 +24,21 @@ contract MyContract {
     function getValue() public pure returns(uint){
         uint value = 1;
         return value;
+    }
+}
+
+contract Arrays {
+    //Arrays
+    uint[] public uintArray = [1,2,3];
+    string[] public stringArray = ["apple", "banana", "carrot"];
+    string[] public values;
+    uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
+
+    function addValue(string memory _value) public{
+        values.push(_value);
+    }
+
+    function valueCount() public view returns(uint) {
+        return values.length;
     }
 }
