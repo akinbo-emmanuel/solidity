3a616fbca768da7ca6e38b277ad8b8f36069b706
akinbo_emmanuel <akinbo.emmanuel@lmu.edu.ng>
2023-03-22 18:26:17 +0100

    Mappings

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -17,7 +17,6 @@ contract Variables {
         uint256 myUint256;
         string myString;
     }
-
     MyStruct public myStruct = MyStruct(1, "Hello World!");
 
     //Local Variables
@@ -41,4 +40,37 @@ contract Arrays {
     function valueCount() public view returns(uint) {
         return values.length;
     }
+}
+
+contract Mappings{
+    mapping(uint => string) public names;
+    mapping(uint => Book) public books;
+    mapping(address => mapping(uint => Book)) public myBooks;    
+
+    struct Book{
+        string title;
+        string author;
+    }
+
+    constructor() {
+        names[1] = "Adam";
+        names[2] = "Bruce";
+        names[3] = "Carl";
+    }
+
+    function addBook(
+        uint _id,
+        string memory _title,
+        string memory _author
+    ) public{
+        books[_id] = Book(_title, _author);
+    }
+
+    function addMyBook(uint _id, string memory _title, string memory _author) public{
+        myBooks[msg.sender][_id] = Book(_title, _author);
+    }
+}
+
+contract ConditionalsAndLoops{
+    
 }
